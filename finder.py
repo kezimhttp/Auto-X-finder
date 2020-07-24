6fbca3580a7727f4b6bba6e52e42457d4a5f9cb2
KeZiM <kezimoffical@gmail.com>
2020-07-24 18:20:06 +0200

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,7 +1,9 @@
 print("Auto X Find | Choose from the menu")
+print(" ")
 print("1. multiply")
 print("2. addition")
 print("3. subtraction")
+print("4. division")
 choose = input()
 choose = int(choose)
 
@@ -36,5 +38,16 @@ if choose == 3:
     score = int(score)
 
     x = score+user_number
-    print("Operation: ",user_number,"-",x,"=",score)
+    print("Operation: ",x,"-",user_number,"=",score)
+    print("x =",x)
+if choose == 4:
+    print("Okay, type your first number")
+    user_number = input()
+    user_number = int(user_number)
+    print("Okay, type result")
+    score = input()
+    score = int(score)
+
+    x = score*user_number
+    print("Operation: ",x,"/",user_number,"=",score)
     print("x =",x)
